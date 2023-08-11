d1fa378295bd995911eed049c82db6dc0d6bd519
Gentianlleshi <33518178+Gentianlleshi@users.noreply.github.com>
2023-08-11 11:23:49 +0200

    import fonts from links

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,17 @@ export default function RootLayout({
   return (
     <html lang="it">
       <Head>
-        <link rel="stylesheet" href="/globals.css" as="style" />
+        <link rel="stylesheet" href="./globals.css" as="style" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body id="root">{children}</body>
     </html>
