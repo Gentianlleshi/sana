8cf27eb5d7234f125c60b01fbc30b339f7cf35e1
Gentianlleshi <33518178+Gentianlleshi@users.noreply.github.com>
2023-08-11 11:51:04 +0200

    fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
     <html lang="it">
       <Head>
         <link rel="stylesheet" href="./globals.css" as="style" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
@@ -25,7 +25,7 @@ export default function RootLayout({
         <link
           href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
-        />
+        /> */}
       </Head>
       <body id="root">{children}</body>
     </html>
